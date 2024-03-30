15821f5e18b9ee24c3ab2a788913515799388f2b
Jason Siefken <siefkenj@gmail.com>
2024-03-29 20:41:29 -0400

    try building

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
-pretext support
+PROJECT_ROOT=$1
+PRETEXT_COMMAND=$2
+OUTPUT_DIR=$3
 
-echo "files here"
-pwd
-ls
+ABSOLUTE_PROJECT_ROOT="$PWD/$PROJECT_ROOT"
+ABSOLUTE_OUTPUT_DIR="$PWD/$OUTPUT_DIR"
+
+echo "Using project root $PROJECT_ROOT"
+pushd "$PROJECT_ROOT"
+
+echo "Running pretext command 'pretext $PRETEXT_COMMAND'"
+pretext $PRETEXT_COMMAND
+
+echo "Moving output/ to $OUTPUT_DIR"
+mv output/ "$ABSOLUTE_OUTPUT_DIR"
+
+popd
+
+echo "Listing files in $OUTPUT_DIR"
+ls "$OUTPUT_DIR"
