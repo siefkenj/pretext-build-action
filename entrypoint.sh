fa27109401a9455f2c8a55a5aadbd70c668be93b
Jason Siefken <siefkenj@gmail.com>
2024-03-29 20:51:01 -0400

    colorful output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Some color variables for pretty echoing
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+GRAY='\033[0;37m'
+NC='\033[0m' # No Color
+
 PROJECT_ROOT=$1
 PRETEXT_COMMAND=$2
 OUTPUT_DIR=$3
@@ -7,16 +14,21 @@ OUTPUT_DIR=$3
 ABSOLUTE_PROJECT_ROOT="$PWD/$PROJECT_ROOT"
 ABSOLUTE_OUTPUT_DIR="$PWD/$OUTPUT_DIR"
 
-echo "Using project root $PROJECT_ROOT"
+echo "${BLUE}Using project root $PROJECT_ROOT${NC}"
 pushd "$PROJECT_ROOT"
 
-echo "Running pretext command 'pretext $PRETEXT_COMMAND'"
+echo "${BLUE}Pretext version infomration${GRAY}"
+pretext support
+echo "${NC}"
+
+
+echo "${BLUE}Running pretext command 'pretext $PRETEXT_COMMAND'${NC}"
 pretext $PRETEXT_COMMAND
 
-echo "Moving output/ to $OUTPUT_DIR"
+echo "${BLUE}Moving output/ to $OUTPUT_DIR${NC}"
 mv output "$ABSOLUTE_OUTPUT_DIR"
 
 popd
 
-echo "Listing files in $OUTPUT_DIR"
+echo "${BLUE}Listing files in $OUTPUT_DIR${NC}"
 ls "$OUTPUT_DIR/output"
